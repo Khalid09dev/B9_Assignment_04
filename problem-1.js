a9a92c779a631cc69c03d76e1903a1ed8bc56420
khalid09dev <mdkhalidhossen@hotmail.com>
2024-03-03 09:28:33 +0600

    added a little bit condition in problem 01

--- problem-1.js
+++ problem-1.js
@@ -1,17 +1,22 @@
 function calculateMoney(ticketSold) {
-    const perTicketPrice = 120;
-    const daroanWagage = 500;
-
-    const perStuffLunchBill = 50;
-    const totalStuffNum = 8;
-
-    const totalStuffLunchBill = perStuffLunchBill * totalStuffNum;
-    const soldedTicketPrices = perTicketPrice * ticketSold;
-
-    const netProfit = soldedTicketPrices - (totalStuffLunchBill + daroanWagage);
-    return netProfit;
+    if(ticketSold < 0) {
+        return '!input have to be positive number';
+    }
+    else {
+        const perTicketPrice = 120;
+        const daroanWagage = 500;
+    
+        const perStuffLunchBill = 50;
+        const totalStuffNum = 8;
+    
+        const totalStuffLunchBill = perStuffLunchBill * totalStuffNum;
+        const soldedTicketPrices = perTicketPrice * ticketSold;
+    
+        const netProfit = soldedTicketPrices - (totalStuffLunchBill + daroanWagage);
+        return netProfit;
+    }
 
 }
-const ticketSale = 500;
+const ticketSale = -10;
 const calculate = calculateMoney(ticketSale);
 console.log(calculate);
