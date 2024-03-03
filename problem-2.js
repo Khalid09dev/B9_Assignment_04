f2df1016f8e146e4affdd0f23d9cbf6368e24db2
khalid09dev <mdkhalidhossen@hotmail.com>
2024-03-03 13:28:23 +0600

    added input type validation system in problem-2

--- problem-2.js
+++ problem-2.js
@@ -1,26 +1,29 @@
-function checkName(name) {
+function checkName(input) {
+    if(typeof(input) !== "string") {
+        return 'invalid';
+    }
+
+    const name = input.toLowerCase();
     const lastChar = name.slice(-1);
-
-    const array = 'aeiouyw';
-    if(array === 'aeiouyw') {
-        for(const arr of array) {
-            if(lastChar === arr) {
-                return 'good name';
-            }
-        }
-
+    
+        const array = 'aeiouyw';
         if(array === 'aeiouyw') {
             for(const arr of array) {
-                if(lastChar !== arr) {
-                    return 'bad name';
+                if(lastChar === arr) {
+                    return 'good name';
+                }
+            }
+    
+            if(array === 'aeiouyw') {
+                for(const arr of array) {
+                    if(lastChar !== arr) {
+                        return 'bad name';
+                    }
                 }
             }
         }
     }
-}
 
-
-
-const inputName = 'melindaA';
-const nameVerify = checkName(inputName.toLocaleLowerCase());
+const inputName = 'tony';
+const nameVerify = checkName(inputName);
 console.log(nameVerify);
